f702c116486a8be95b2e3a5af69a26e64bf43457
Matt Maher <mattmaher0@gmail.com>
2018-05-15 14:59:49 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,5 +1,14 @@
 def begins_with_r(array)
-  array.all?
+  array.each do |word|
+    if word[0] == "r"
+      word = true
+    end  
+   end
+    if array.all?(true)
+      return true
+    else
+      return false
+    end  
 end  
 
 def contain_a
